0585bec3ce6524c631fb8bab5dc775e9004aa630
Daniel <daniel.bencz@gmail.com>
2020-12-28 09:30:06 +0100

    first commit

---
+++ precache-manifest.dd25494e108aea745ef58a2cab832c11.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28cabd20cb2ffedb60bc98791608077e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "457a9e447e1d52387623",
+    "url": "/static/css/main.5f3a2709.chunk.css"
+  },
+  {
+    "revision": "a6e4d656761501483f78",
+    "url": "/static/js/2.583b2fcd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.583b2fcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "457a9e447e1d52387623",
+    "url": "/static/js/main.aa351080.chunk.js"
+  },
+  {
+    "revision": "fa504d9b7a15b9783a7e",
+    "url": "/static/js/runtime-main.ef85d0e0.js"
+  },
+  {
+    "revision": "85ad91403565c32dcc7669675f9a62fd",
+    "url": "/static/media/github.85ad9140.png"
+  },
+  {
+    "revision": "b1f04edacab774d5b9fcfba458a907a3",
+    "url": "/static/media/share-100.b1f04eda.png"
+  }
+]);
